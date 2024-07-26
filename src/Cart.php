7ae07f3d6dadfb0664e0a7b16225fd9986030da9
Jordy <58026189+Undermozes@users.noreply.github.com>
2024-07-26 16:39:27 +0200

    Enable foreign key constraints

--- src/Cart.php
+++ src/Cart.php
@@ -199,6 +199,10 @@ class Cart
      */
     private function deleteCartRecord($identifier)
     {
+        // Enable foreign key constraints
+        $this->getConnection()->statement('PRAGMA foreign_keys = ON;');
+
+
         $this->getConnection()->table($this->getTableName())
             ->where('identifier', $identifier)
             ->delete();
